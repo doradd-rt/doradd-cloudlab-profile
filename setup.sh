b1dcb5f5e0610e1230cfc9b7c27421ea921b8d17
Scofield626 <scofield_zliu@outlook.com>
2024-11-21 19:30:59 +0000

    fix: correct write path

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
+#!/bin/bash
+set -x
+SCRIPTNAME=$0
+#
+# Might not be on the local cluster, so need to use the urn to
+# see who the actual creator is.
+#
+GENIUSER=`geni-get user_urn | awk -F+ '{print $4}'`
+if [ $? -ne 0 ]; then
+echo "ERROR: could not run geni-get user_urn!"
+exit 1
+fi
+
 sudo apt update
 sudo apt install -y meson python3-pyelftools cmake pkg-config
 
-wget https://content.mellanox.com/ofed/MLNX_OFED-24.04-0.6.6.0/MLNX_OFED_LINUX-24.04-0.6.6.0-ubuntu22.04-x86_64.tgz --no-check-certificate
+pushd /home/$GENIUSER 
+sudo su -c $GENIUSER -c "wget https://content.mellanox.com/ofed/MLNX_OFED-24.04-0.6.6.0/MLNX_OFED_LINUX-24.04-0.6.6.0-ubuntu22.04-x86_64.tgz --no-check-certificate"
 tar -xvzf MLNX_OFED_LINUX-24.04-0.6.6.0-ubuntu22.04-x86_64.tgz
 pushd MLNX_OFED_LINUX-24.04-0.6.6.0-ubuntu22.04-x86_64
 sudo ./mlnxofedinstall --force --upstream-libs --dpdk
 sudo /etc/init.d/openibd restart
+popd
 popd
 
 # TODO: clone ppopp-artifact repo to make it consistent and unaffected by future changes
